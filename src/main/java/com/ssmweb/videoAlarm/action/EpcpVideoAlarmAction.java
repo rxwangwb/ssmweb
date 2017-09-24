c7fe75e627adf1aacb72767f95b766baefd6c61a
rxwangwb <rxwangwb@gmail.com>
2017-09-24 10:59:16 +0800

    整合spring,struts,mybatis

--- src/main/java/com/ssmweb/videoAlarm/action/EpcpVideoAlarmAction.java
+++ src/main/java/com/ssmweb/videoAlarm/action/EpcpVideoAlarmAction.java
@@ -1,8 +1,11 @@
 package com.ssmweb.videoAlarm.action;
 
+import com.ssmweb.videoAlarm.pojo.EpcpVideoAlarm;
 import com.ssmweb.videoAlarm.service.EpcpVideoAlarmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class EpcpVideoAlarmAction {
@@ -11,7 +14,8 @@ public class EpcpVideoAlarmAction {
 
     public String selectEpcpVideoAlarms() {
         System.out.println("selectEpcpVideoAlarms" + "++++++++++++++");
-        videoAlarmService.selectEpcpVideoAlarms();
+        List<EpcpVideoAlarm> videoAlarmList = videoAlarmService.selectEpcpVideoAlarms();
+        System.out.println(videoAlarmList.size());
         return "SUCCESS";
     }
 }
--- src/main/java/com/ssmweb/videoAlarm/dao/EpcpVideoAlarmDao.java
+++ src/main/java/com/ssmweb/videoAlarm/dao/EpcpVideoAlarmDao.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface EpcpVideoAlarmMapper {
+public interface EpcpVideoAlarmDao {
     List<EpcpVideoAlarm> selectEpcpVideoAlarms();
 }
